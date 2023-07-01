aa66fb93774687c081eb1cf8b55dc5a666810858
hamidreza <hamidreza.lipar@gmail.com>
2023-07-01 18:00:12 +0330

    before send

--- src/App.js
+++ src/App.js
@@ -2,46 +2,82 @@ import React, { Component } from 'react';
 import Home from './components/Home'
 import Aboutus from './components/Aboutus'
 // import Movies from './components/Movies'
-// import Aside from './components/Aside'
+ import Aside from './components/Aside'
 import Movie from './components/Movie'
 import style from './styles/style.module.css'
 import './styles/mystyle.scss'
 import axios from "axios";
-
 import { Link, Routes, Route } from 'react-router-dom'
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       movies: [],
-      names: [],
+      temple: [],
       Genres: [],
+      Genres2:[],
+      btngenre:[],
     }
-
-
     axios.get('./json/movies.json')
-      .then(res => this.setState({ movies: res.data }))
-      .then(console.log(this.state.movies))
-
+    .then(res => this.setState({ movies: res.data, temple: res.data }))
   }
-  setnames = ()=>{
-    var movienames=[]; 
-      this.state.movies.map(movie=>movienames.push(movie.Title))
-       this.setState({names:movienames})
-  }
-  changehandler=(event)=>{
-    const temple=this.state.movies;
+  search = (event) => {
+    const {movies}=this.state;
     const targets = [];
-    for (let index = 0; index < this.state.movies.length; index++) {
-      const element = this.state.movies[index];
-      if (element.Title.includes(event.target.value)){targets.push(element)}
+    for (let index = 0; index < movies.length; index++) {
+        const element = movies[index];
+        if (element.Title.toLowerCase().includes(event.target.value)) { targets.push(element) }
     }
-    console.log(temple)
-    console.log(targets)
-    this.setState({movies:targets})
+    this.setState({ movies: targets })
+}
+setter=()=>{
+    this.setState({ movies: this.state.temple})
+}
+/////////////////////////////////////////////////////////////////////////////////////
+setgenres=()=>{
+  let array1 = this.genrestoarray();
+  let array2 = this.clearwhitespace(array1);
+  let array3 = this.filter(array2);
+  this.setState({ Genres:array3})
+}
 
+genrestoarray = () => {
+  let str = "";
+  var array = [];
+  for (let index = 0; index < this.state.temple.length; index++) {
+      const element = this.state.temple[index].Genre;
+      str += ("," + element);
+      array = str.split(",");
+      array.shift();
   }
-  
+  return array;
+}
+clearwhitespace = (array) => {
+  let array2 = [];
+  for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      const element2 = element.trim();
+      array2.push(element2)
+  }
+  return array2;
+}
+filter = (array) => {
+  let array2 = array.filter((c, index) => {
+      return array.indexOf(c) === index;
+  });
+  return array2;
+}
+btngenre=(genre)=>{
+  var target=[];
+  this.state.temple.map((movie)=>{
+    let array = movie.Genre.split(",")
+    var arr1 = this.clearwhitespace(array)
+     if(arr1.includes(genre)){
+       target.push(movie)
+      }
+    })
+    this.setState({movies:target})  
+}
   render() {
 
     return (
@@ -64,21 +100,7 @@ class App extends Component {
                   <Route path="/Movies" element={<div className="movie-container">{this.state.movies.map(film => <Movie key={film.Id} poster={film.Poster} title={film.Title} year={film.Year} director={film.Director} country={film.Country} genre={film.Genre} />)}</div>}></Route>
                 </Routes>
               </main>
-              <aside className="aside">
-                <label htmlFor="">Search here :</label><br />
-                <input type="text" onFocus={this.setnames} onChange={this.changehandler}/>
-                <hr />
-                <div className="filters">
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                </div>
-              </aside>
+              <Aside className="aside"search = {this.search} setter={this.setter} Genres={this.state.Genres} btngenre={this.btngenre} setgenres={this.setgenres} />
             </div>
 
           </div>
--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react'
 
- class Aside extends Component {
+class Aside extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+
+    }
+    changehandler = (event) => {
+        const { search, setter } = this.props;
+        if (event.keyCode < 97) { event.keyCode += 32 }
+        if (event.keyCode === 40) { setter(); }
+        setTimeout(() => { search(event); }, 5);
+    }
+    focus = (event)=>{
+
+        this.props.setgenres();
+    }
+    blur = (event)=>{
+        event.target.value=""
+        this.props.setter();
+    }
     render() {
+        const{Genres , btngenre}=this.props;
         return (
             <>
                 <aside className="aside">
-                <label htmlFor="">Search here :</label><br />
-                <input type="text" />
-                <hr />
-                <div className="filters">
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                  <button>dram</button>
-                </div>
-              </aside>
+                    <label htmlFor="">Search here :</label><br />
+                    <input type="text" onKeyUp={this.changehandler} onFocus={this.focus} onBlur={this.blur}/>
+                    <hr />
+                    <div className="filters">
+                    {Genres.map((genre,i=1)=>(<button key={i++}  onClick={() => {btngenre(`${genre}`) }} >{genre}</button>))}
+                    </div>
+                </aside>
             </>
         )
     }
